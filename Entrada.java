07d178e0683af54a89751628e0d05b50960bafb0
MajorCampe <lucascampesatto@gmail.com>
2015-06-18 15:18:01 -0400

    Update Entrada.java

--- Entrada.java
+++ Entrada.java
@@ -9,23 +9,29 @@ private String retornaTexto(){
     return txt;
 }
 
-private String retornaTextoCompleto(){
-    String txt = null;
-    try{
+private String retornaTextoCompleto() {
+        String txt = null;
+        try {
 
-        InputStream is = new FileInputStream("arquivo.txt");
-        InputStreamReader isr = new InputStreamReader(is);
-        BufferedReader br = new BufferedReader(isr);
-        for (String i = br.readLine(); i != null; i = br.readLine()){
-            String[] tokens = i.split(" ");
-            for (String token : tokens){
-                txt.concat(token);
+            InputStream is = new FileInputStream("arquivo.txt"); // Cria uma instancia de InputStream do tipo "File" que recebe um arquivo a ser lido.
+            InputStreamReader isr = new InputStreamReader(is); //Cria um StreamReader que recebe o ImputStream criado acima por parametro.
+            BufferedReader br = new BufferedReader(isr); //Cria um BufferedReader recebendo o StreamReader por parametro.
+
+            /* Faz um "for" percorrendo todas as possíveis linhas de entrada o BufferedReader */
+            for (String i = br.readLine(); i != null; i = br.readLine()) {
+
+                String[] tokens = i.split(" "); // Acumula cada palavra linha em um vetor de Strings chamado tokens.
+
+                for (String token : tokens) { //"for" que percorre a coleção (vetor de string) 
+                    txt.concat(token); //Junta cada string do vetor em uma String só, para criar o retorno de saida do metodo.
+
+
+                }
             }
+
+
+        } catch (IOException ex) {
+            System.out.println(ex); //Imprime no console uma possivel exception
         }
-
-
-    }catch (IOException ex){
-        System.out.println(ex);
+        return txt; //Retorna a string lida do arquivo para quem chamou o metodo.
     }
-    return txt;
-}
