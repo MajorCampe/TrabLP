48075e713817fbf89c1265365d8a11435fec3ff1
MajorCampe <lucascampesatto@gmail.com>
2015-06-18 15:20:06 -0400

    Update Entrada.java

--- Entrada.java
+++ Entrada.java
@@ -1,12 +1,15 @@
-private String retornaTexto(){
-    try{
-        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("arquivo.txt")));
-        String txt = br.readLine();
-    }
-    catch(IOException ex){
-        System.out.println(ex);
-    }
-    return txt;
+private String retornaTexto(){ 
+    try { // encapsulamento para tratar uma possivel exception
+
+    /* Cria uma variável de BufferdReader chamada "br" que recebe uma nova instância de BufferedReaderm que por sua vez recebe por paramentro uma nova instância de InputStreamReader (o leitor padrão de stream de entrada), que por sua vez recebe por parâmetro uma nova instância de FileInputStream (classe responsável pelo tratamento de entrada de arquivos) recebendo por parametro o nome do arquivo a ser lido. */
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("arquivo.txt"))); 
+
+
+        String txt = br.readLine(); // lê a linha do arquivo e guarda como String na variavel txt
+    } catch (IOException ex) {
+                   System.out.println(ex); // imprime no console a exception que por ventura ocorreu.
+        }
+    return txt; //retorno do metodo, joga a string txt para quem chamou o metodo.
 }
 
 private String retornaTextoCompleto() {
